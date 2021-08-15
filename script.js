d23d64d71e45d4422fd3c9400f4e596a052237be
AnaPutkaradze <85496856+AnaPutkaradze@users.noreply.github.com>
2021-08-16 00:36:24 +0400

    add ajax

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ let togglenutton = document.getElementById('toggleButton');
 toggleButton.addEventListener('click', function() {
     navbarLinks.classList.toggle('active');
 })
+
+
+// XMLHttpRequest example
+function getComments () {
+
+    function render () {
+        let response = this.responseText;
+        let responseData = JSON.parse(response);
+        console.log(responseData);
+
+        let ul = document.createElement('ul');
+
+        responseData.data.forEach(item => {
+            let li = document.createElement('li');
+            li.textContent = item.first_name + ' ' + item.last_name;
+
+            ul.appendChild(li);
+        });
+
+        document.getElementById('api').appendChild(ul);
+    }
+    let requist = new XMLHttpRequest();
+    requist.addEventListener('load', render);
+    
+    requist.open('GET', 'https://reqres.in/api/users?page=2');
+    requist.send();
+}
+
+getComments();
