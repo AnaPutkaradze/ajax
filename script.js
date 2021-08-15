a75d6036879aea9e86fc19f1a279c58a02b499a9
AnaPutkaradze <85496856+AnaPutkaradze@users.noreply.github.com>
2021-08-16 00:25:14 +0400

    add burger

---
+++ script.js
@@ -0,0 +1,6 @@
+let navbarLinks = document.getElementById('navbarLinks');
+let togglenutton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('click', function() {
+    navbarLinks.classList.toggle('active');
+})
